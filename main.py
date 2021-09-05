ec63bc1ddc28cb7ae3a29b20f980f6ebb26754ca
harishgl2007 <67889880+harishgl2007@users.noreply.github.com>
2021-09-05 11:04:12 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# HARISH DEV 
+
+while True: #loop
+ x = input("Enter table number:")
+ x = int(x)#Getting Input and Intilize it as a varible
+
+ if x == 0:
+     print("0 is not a valid number")#If the input = 0 break the loop
+     break
+     
+
+ print(x,"*1=",x*1)
+ print(x,"*2=",x*2)
+ print(x,"*3=",x*3)
+ print(x,"*4=",x*4)
+ print(x,"*5=",x*5)
+ print(x,"*6=",x*6)
+ print(x,"*7=",x*7)
+ print(x,"*8=",x*8)
+ print(x,"*9=",x*9)
+ print(x,"*10=",x*10)
+ print(x,"*11=",x*11)
+ print(x,"*12=",x*12)
+ #multipy into number and print it
+
